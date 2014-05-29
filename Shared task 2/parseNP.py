ece36ea110d7e9ccd45097124ab920b585f3815d
Philipp Dowling <philipp.dowling@quartett-mobile.de>
2014-05-29 20:08:55 +0200

    messing around

--- Shared task 2/parseNP.py
+++ Shared task 2/parseNP.py
@@ -9,6 +9,7 @@ S -> NP VP
 NP -> N | Det N | Det Adj N | N PP | Det N PP | Det Adj N PP
 NP -> NP conj NP
 
+NP -> N | N PP
 PP -> P | P NP
 
 
@@ -22,32 +23,9 @@ def ie_preprocess(document):
     sentences = [nltk.pos_tag(sent) for sent in sentences]
     return sentences
 
-def npchunk_features(sentence, i, history):
-    word, pos = sentence[i]
-    if i == 0:
-        prevword, prevpos = "<START>", "<START>"
-    else:
-        prevword, prevpos = sentence[i-1]
-    if i == len(sentence)-1:
-        nextword, nextpos = "<END>", "<END>"
-    else:
-        nextword, nextpos = sentence[i+1]
-    return {"pos": pos,
-            "word": word,
-            "prevpos": prevpos,
-            "nextpos": nextpos,
-            "prevpos+pos": "%s+%s" % (prevpos, pos),  
-            "pos+nextpos": "%s+%s" % (pos, nextpos),
-            "tags-since-dt": tags_since_dt(sentence, i)}
 
-def tags_since_dt(sentence, i):
-    tags = set()
-    for word, pos in sentence[:i]:
-        if pos == 'DT':
-            tags = set()
-        else:
-            tags.add(pos)
-    return '+'.join(sorted(tags))
+
+
 
 def read_file(filename):  # this looks a bit messy but it works like a charm
     with open(filename) as f:
@@ -93,16 +71,49 @@ def merge_chunks(sentence):
     for term, pos, bracket in sentence:
         if bracket == "_":
             res.append((term, pos))
-        else: 
+        else:
+            if bracket[0] == "(":
+                pass
+            if bracket[:-1] == ")":
+                pass
             remember = bracket
             chunk = []
 
 
 def rebuild_sentence(sentence):  #fuckyeah
-    return "".join([(" "+ token if token[0] not in list(".,?'\"!") else token) for token, pos in sentence])[1:]
+    return "".join([(" " + token if token[0] not in list(".,?'\"!") else token) for token, pos, _ in sentence])[1:]
+
 
 def write_results(results, outfile):
     pass
+
+
+def npchunk_features(sentence, i, history):
+    word, pos = sentence[i]
+    if i == 0:
+        prevword, prevpos = "<START>", "<START>"
+    else:
+        prevword, prevpos = sentence[i-1]
+    if i == len(sentence)-1:
+        nextword, nextpos = "<END>", "<END>"
+    else:
+        nextword, nextpos = sentence[i+1]
+    return {"pos": pos,
+            "word": word,
+            "prevpos": prevpos,
+            "nextpos": nextpos,
+            "prevpos+pos": "%s+%s" % (prevpos, pos),  
+            "pos+nextpos": "%s+%s" % (pos, nextpos),
+            "tags-since-dt": tags_since_dt(sentence, i)}
+
+def tags_since_dt(sentence, i):
+    tags = set()
+    for word, pos in sentence[:i]:
+        if pos == 'DT':
+            tags = set()
+        else:
+            tags.add(pos)
+    return '+'.join(sorted(tags))
 
 class ConsecutiveNPChunkTagger(nltk.TaggerI):
     def __init__(self, train_sents):
@@ -112,7 +123,7 @@ class ConsecutiveNPChunkTagger(nltk.TaggerI):
             history = []
             for i, (word, tag) in enumerate(tagged_sent):
                 featureset = npchunk_features(untagged_sent, i, history)
-                train_set.append( (featureset, tag) )
+                train_set.append((featureset, tag))
                 history.append(tag)
         #self.classifier = nltk.MaxentClassifier.train(train_set, algorithm='iis', trace=0)  # megam
         self.classifier = nltk.classify.NaiveBayesClassifier.train(train_set)
@@ -125,17 +136,56 @@ class ConsecutiveNPChunkTagger(nltk.TaggerI):
             history.append(tag)
         return zip(sentence, history)
 
-
 class ConsecutiveNPChunker(nltk.ChunkParserI):
     def __init__(self, train_sents):
-        tagged_sents = [[((word,tag),chunk) for (word,tag,chunk) in nltk.chunk.tree2conlltags(sent)] for sent in train_sents]
+        tagged_sents = [[((word, tag), chunk) for (word, tag, chunk) in nltk.chunk.tree2conlltags(sent)] for sent in train_sents]
         self.tagger = ConsecutiveNPChunkTagger(tagged_sents)
 
     def parse(self, sentence):
         tagged_sents = self.tagger.tag(sentence)
         conlltags = [(w,t,c) for ((w,t),c) in tagged_sents]
+        print "conlltags are: %s" % conlltags
         return nltk.chunk.util.conlltags2tree(conlltags)
 
+def get_tag(chunk):
+    return chunk.node if hasattr(chunk, "node") else chunk[1]
+
+def recursive_np_chunk_features(treelist, i, history):
+    # treelist: 
+    #[
+    # ('NP', Tree('NP', [('The', 'DT'), ('quick', 'NN'), ('brown', 'NN'), ('fox', 'NN')])),
+    # ('IN', ('from', 'IN')), 
+    # ('NP', Tree('NP', [('mars', 'NNS')])), 
+    # ('VBD', ('jumped', 'VBD')), 
+    # ('IN', ('over', 'IN')),
+    # ('NP', Tree('NP', [('the', 'DT'), ('fence', 'NN')])), 
+    # ('.', ('.', '.'))
+    #]
+    tag, content = treelist[i]
+    if i == 0:
+        prevtag, prevcontent = "<START>", "<START>"
+    else:
+        prevtag, prevcontent = treelist[i-1]
+    if i == len(treelist)-1:
+        nexttag, nextcontent = "<END>", "<END>"
+    else:
+        nexttag, nextcontent = treelist[i+1]
+    return {"tag": tag,
+            "prevtag": prevtag,
+            "nexttag": nexttag,
+            "prevpos+pos": "%s+%s" % (prevpos, pos),  
+            "pos+nextpos": "%s+%s" % (pos, nextpos),
+            "tags-since-dt": tags_since_dt(sentence, i)}
+
+
+
+class RecursiveNPChunkTagger(nltk.TaggerI):
+    def __init__(self, train_chunks):
+        train_set = []
+        for tagged_chunk in train_chunks:
+            history = []
+            for i, (chunk, tree) in enumerate(tagged_chunk):
+                featureset = recursive_np_chunk_features(tree, i, history)
 
 
 c = ConsecutiveNPChunker(train_sents)
